bfc2972b06daa73e7819d1baba51bd83af61f419
Kamrul Hasan <129537477+kamrulkakon@users.noreply.github.com>
2023-11-15 10:54:28 +0600

    create entity class

---
+++ src/main/java/com/example/upload_download/Entity/upload_download_entity.java
@@ -0,0 +1,22 @@
+package com.example.upload_download.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "upload_download")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class upload_download_entity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String profile;
+    private long size;
+    private byte[] content;
+}
